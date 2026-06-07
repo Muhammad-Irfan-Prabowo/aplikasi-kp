9902d757bcbca3e63d393d702cb3a16c8ca4d120
M Irfanprabowo <if22.muhammadprabowo@mhs.ubpkarawang.ac.id>
2026-06-07 19:36:23 +0700

    Update app.js
    Perbarui kalimat contoh

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send(`
         <div style="text-align: center; font-family: Arial, sans-serif; margin-top: 50px;">
-            <h1 style="color: #34a853;">Contoh Penerapan Cloud Computing - Dicoding Kampus Merdeka</h1>
+            <h1 style="color: #34a853;">Penerapan Cloud Computing - Dicoding Kampus Merdeka</h1>
             <h2>Aplikasi Web Berhasil Dideploy di Cloud Computing!</h2>
             <hr style="width: 50%; margin: 20px auto;">
             <p><strong>Nama:</strong> [Muhammad Irfan Prabowo]</p>
