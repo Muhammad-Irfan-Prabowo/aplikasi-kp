0ed7638e8df3ecce1a8ac05649c29e7e4c6257d4
M Irfanprabowo <if22.muhammadprabowo@mhs.ubpkarawang.ac.id>
2026-07-08 15:02:06 +0700

    Update app.js
    ok

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
             <p><strong>Nama:</strong> [Muhammad Irfan Prabowo]</p>
             <p><strong>Nama:</strong> [Universitas Buana Perjuangan Karawang]</p>
             <p><strong>Program:</strong> Cloud Computing - MSIB Dicoding</p>
-            <p><strong>Status Server:</strong> Aktif dan Berjalan Lancar</p>
+            <p><strong>Status Server:</strong> Aktif dan Berjalan Lancar ok</p>
         </div>
     `);
 });
